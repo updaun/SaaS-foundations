0610ea2c2449a87fb3c1510c6988ddca64980439
updaun <updauney@daum.net>
2025-01-15 22:58:30 +0900

    chore: Navbar Links for Auth

--- src/config/urls.py
+++ src/config/urls.py
@@ -21,7 +21,7 @@ from auth import views as auth_views
 from .views import home_view, about_view
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name="home"),
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
     path("about/", about_view),
