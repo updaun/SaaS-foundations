be4ad6b1f5abdf2dc8df0b85afc05b5313fcb7f7
updaun <updauney@daum.net>
2025-01-21 21:42:37 +0900

    chore: User-required Pages via login_required Decorator

--- src/config/views.py
+++ src/config/views.py
@@ -1,5 +1,6 @@
 import pathlib
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from visits.models import PageVisit
 
@@ -7,6 +8,8 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 
 def home_view(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        print(request.user.first_name)
     return about_view(request, *args, **kwargs)
 
 
@@ -42,3 +45,8 @@ def pw_protected_view(request, *args, **kwargs):
     if is_allowed:
         return render(request, "protected/view.html", {})
     return render(request, "protected/entry.html", {})
+
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html", {})
