78b0a62d6c424d9e9fe0285f19edb6193e082ee5
updaun <updauney@daum.net>
2025-01-23 20:46:08 +0900

    chore: Subscription Pricing View & Template

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
+from subscriptions import views as subscriptions_views
 from .views import (
     home_view,
     about_view,
@@ -28,8 +29,9 @@ from .views import (
 
 urlpatterns = [
     path("", home_view, name="home"),
-    path("login/", auth_views.login_view),
-    path("register/", auth_views.register_view),
+    # path("login/", auth_views.login_view),
+    # path("register/", auth_views.register_view),
+    path("pricing/", subscriptions_views.subscription_price_view, name="pricing"),
     path("about/", about_view),
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
